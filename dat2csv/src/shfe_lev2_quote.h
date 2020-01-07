70521a951c83414ad16701ac6e459de188a970de
wangying <17199883@qq.com>
2020-01-07 10:59:24 +0800

    dd

---
+++ dat2csv/src/shfe_lev2_quote.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include "my_quote_save.h"
+#include "ThostFtdcUserApiStruct.h"
+
+
+std::string ShfeLev2QuoteToString(int serail_number, const SaveData_ShfeLev2Data* const p);
+
+
