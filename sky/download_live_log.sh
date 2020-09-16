17687866a916664eee785d872910a100855e9f40
wangying <17199883@qq.com>
2020-09-16 16:18:53 +0800

    ddd

--- sky/download_live_log.sh
+++ sky/download_live_log.sh
@@ -34,15 +34,19 @@ if [ $? -ne 0 ]; then
 	echo "" | mail -s "异常：没有发现so的日志文件，可能原因：CTP初始化问题，请检查！！！" 17199883@qq.com
 
 	ssh -p 44163  u910019@101.231.3.117 "pkill -SIGUSR2 y-trade"
-	cat "${last_log}" | mail -s "关闭交易程序并重启。。。" 17199883@qq.com
+	cat "" | mail -s "关闭交易程序并重启。。。" 3580771905@qq.com
+	cat "" | mail -s "关闭交易程序并重启。。。" 17199883@qq.com
 
 	ssh -p 44163  u910019@101.231.3.117 "sh /home/u910019/yao/trade/ytrader/start_only.sh" &
 	echo "交易程序重启完成。"
+	echo "" | mail -s "交易程序重启完成。" 3580771905@qq.com
 	echo "" | mail -s "交易程序重启完成。" 17199883@qq.com
 
 	echo "交易程序重启完成。"
 
+	scp  -P 44163  "u910019@101.231.3.117:${STRATEGY_LOG}" ./
 
 fi
+
 
 echo "done!!!"
