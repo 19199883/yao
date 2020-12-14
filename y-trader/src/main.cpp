6f9322423033090cd217e6296284add35b6c3387
wangying <17199883@qq.com>
2020-12-14 16:19:09 +0800

    1. expand order capacity to 30000

--- y-trader/src/main.cpp
+++ y-trader/src/main.cpp
@@ -44,7 +44,7 @@ int main(/*int argc, const char **argv*/)
 	sigaction(SIGUSR2, &SIGINT_act, NULL);
 
 	// clog setting		   CLOG_LEVEL_WARNING
-	clog_set_minimum_level(CLOG_LEVEL_INFO);
+	clog_set_minimum_level(CLOG_LEVEL_WARNING);
 	FILE *fp;/*文件指针*/
 	fp=fopen("./x-trader.log","w+");
 
@@ -73,7 +73,7 @@ int main(/*int argc, const char **argv*/)
 	clog_warning("PERSISTENCE_ENABLEDon off"); 
 #endif
 	// version
-	clog_warning("version:ytrader_2020-10-20"); 
+	clog_warning("version:ytrader_2020-11-15"); 
 	
 	struct vrt_queue  *queue;
 	int64_t  result;
