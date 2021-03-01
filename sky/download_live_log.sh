d83dea737fc0d23a2973013bf7f1d9552d398733
wangying <17199883@qq.com>
2021-03-01 19:34:41 +0800

    ddd

--- sky/download_live_log.sh
+++ sky/download_live_log.sh
@@ -30,22 +30,23 @@ cd "strategy-log"
 STRATEGY_LOG="/home/u910019/yao/trade/ytrader/log/*.log"
 scp  -P 44163  "u910019@101.231.3.117:${STRATEGY_LOG}" ./
 if [ $? -ne 0 ]; then
-	echo "" | mail -s "异常：没有发现so的日志文件，可能原因：CTP初始化问题，请检查！！！" 3580771905@qq.com
-	echo "" | mail -s "异常：没有发现so的日志文件，可能原因：CTP初始化问题，请检查！！！" 17199883@qq.com
+	echo "" | mail -s "异常：没有发现so的日志文件，可能原因：CTP初始化问题，将重启交易系统. 请检查！！！" 3580771905@qq.com
+	sleep 10
+	echo "" | mail -s "异常：没有发现so的日志文件，可能原因：CTP初始化问题，将重启交易系统. 请检查！！！" 17199883@qq.com
 
 	ssh -p 44163  u910019@101.231.3.117 "pkill -SIGUSR2 y-trade"
-	cat "" | mail -s "关闭交易程序并重启。。。" 3580771905@qq.com
-	cat "" | mail -s "关闭交易程序并重启。。。" 17199883@qq.com
+#cat "" | mail -s "关闭交易程序并重启。。。" 3580771905@qq.com
+#cat "" | mail -s "关闭交易程序并重启。。。" 17199883@qq.com
 
 	ssh -p 44163  u910019@101.231.3.117 "nohup sh /home/u910019/yao/trade/ytrader/start_only.sh >/dev/null 2>&1 &" 
 	
 	echo "交易程序重启完成。"
-	echo "" | mail -s "交易程序重启完成。" 3580771905@qq.com
-	echo "" | mail -s "交易程序重启完成。" 17199883@qq.com
+#echo "" | mail -s "交易程序重启完成。" 3580771905@qq.com
+#echo "" | mail -s "交易程序重启完成。" 17199883@qq.com
 
 	echo "交易程序重启完成。"
 
-	 sleep 15
+	 sleep 30
 
 
 	scp  -P 44163  "u910019@101.231.3.117:${STRATEGY_LOG}" ./
